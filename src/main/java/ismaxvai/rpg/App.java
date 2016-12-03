0c6d3f68131f7ff95f7a11bf0e4379f4bd8dca0e
John Doe <john.doe@abiquo.com>
2016-12-03 15:09:12 +0100

    Dice implementation finished

--- src/main/java/ismaxvai/rpg/App.java
+++ src/main/java/ismaxvai/rpg/App.java
@@ -4,26 +4,13 @@ import org.telegram.telegrambots.api.objects.Update;
 import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 
 
-public class App extends TelegramLongPollingBot
+public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	String tirada = "1d90";
+    	Dice dau = new Dice();
+        System.out.println( dau.fullRolling(tirada) );
     }
 
-	public String getBotUsername() {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	public void onUpdateReceived(Update arg0) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public String getBotToken() {
-		// TODO Auto-generated method stub
-		return null;
-	}
 }
