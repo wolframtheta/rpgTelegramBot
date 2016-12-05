76f43ced02dbf9711005c7df0f9a33d852851880
Xavier Marques <xaviermarques4f@gmail.com>
2016-12-05 17:16:58 +0100

    Revert db settings

--- src/main/java/ismaxvai/rpg/Rpgbot.java
+++ src/main/java/ismaxvai/rpg/Rpgbot.java
@@ -13,10 +13,10 @@ import org.telegram.telegrambots.generics.UpdatesHandler;
 public class Rpgbot {
 	static String USERNAME = "xvaibot";
 	static String TOKEN = "253251073:AAFT1DnXYpVdzfNS97waRT-Jr4VFWuwSxFw";
-	public static final String url = "jdbc:mariadb://hrr5mwqn9zs54rg4.chr7pe7iynqr.eu-west-1.rds.amazonaws.com/vsvpm40pyrm7dvnc";
+	public static final String url = "jdbc:mariadb://localhost/rpg";
     public static final String controllerDB = "com.mysql.jdbc.Driver";
-    public static final String username = "ut88tvlfk6byjr9u";
-    public static final String password = "r4tfq8212p9uptej";
+    public static final String username = "root";
+    public static final String password = "root";
 	public static void main( String[] args ) throws SQLException {
 
 		Connection connection = DriverManager.getConnection(url, username, password);
